2ff085c97e1bf208f54561a9e619824ff9c27b38
Katelind Phillips <scarlettkate94@gmail.com>
2022-06-10 11:38:16 -0400

    first class exercise

---
+++ classEx1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace classEx1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car firstCar = new Car();
+
+            firstCar.Make = "Toyota";
+            firstCar.Model = "Rav4";
+            firstCar.Year = 2020;
+
+            Console.WriteLine($"My car is a {firstCar.Year} {firstCar.Make} {firstCar.Model}.");
+        }
+    }
+}
+
